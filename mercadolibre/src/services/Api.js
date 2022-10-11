c9d19cf7ae5e4968857300986aff4cb2f9b2ae7c
mlopez007 <46654705+mlopez007@users.noreply.github.com>
2022-10-11 08:34:56 -0300

    fix - remove baseUrl of all endpoint

--- mercadolibre/src/services/Api.js
+++ mercadolibre/src/services/Api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
 export const getSearch = async (q) => {
   try {
-    const response = await axios(
-      "http://localhost:3000/api/items", //http://localhost:3000/api/items
-      {
-        params: { q },
-      }
-    );
+    const response = await api.get("/api/items", {
+      params: { q },
+    });
     return response;
   } catch (error) {
     console.log("error: ", error);
@@ -17,7 +18,7 @@ export const getSearch = async (q) => {
 
 export const getProductById = async (id) => {
   try {
-    const response = await axios(`http://localhost:3000/api/items/${id}`);
+    const response = await api.get(`/api/items/${id}`);
     return response;
   } catch (error) {
     console.log("error: ", error);
